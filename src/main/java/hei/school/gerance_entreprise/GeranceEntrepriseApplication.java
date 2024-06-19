27f44a4531fda1d66fab57a3609446babe1cd059
NanteRanaivosoa <ranaivosoamami@gmail.com>
2024-06-19 14:49:47 +0200

    first commit

---
+++ src/main/java/hei/school/gerance_entreprise/GeranceEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package hei.school.gerance_entreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeranceEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeranceEntrepriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hei/school/gerance_entreprise/GeranceEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.gerance_entreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeranceEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
